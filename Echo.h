a7c4efe6c02246d4f814122412dce888948669a7
Paul J. Diwiak <diwiak@student.tugraz.at>
2017-04-24 20:30:43 +0200

    now with echo.cpp and echo.h

---
+++ Echo.h
@@ -0,0 +1,9 @@
+//------------------------------------------------------------------------------
+// Filename: Echo.h
+// Description: header-file for the echo command
+// Authors: Alexander Brunner, Paul J. Diwiak, Martin Sackl
+// Tutor: Roman Walch
+// Group: 15626
+// Created: 21.03.2017
+// Last change: 30.03.2017
+//-----------------------------------------------------------------------------
