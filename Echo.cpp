95582d0932fe7c2e143763d7d8d36d27fbfd3952
Martin Sackl <martin.sackl@student.tugraz.at>
2017-04-25 19:02:33 +0200

    Echo and Recipe functionality workes

--- Echo.cpp
+++ Echo.cpp
@@ -21,5 +21,9 @@ Echo::Echo(std::string name) : Command(name)
 // int Echo::execute(GameHandler& game, std::vector<std::string>& params)
 int Echo::execute(std::vector<std::string>& params)
 {
+  for(int i = 0; i < params.size(); i++)
+  {
+    std::cout << params[i] << std::endl;
+  }
   return 0;
 }
--- Recipe.cpp
+++ Recipe.cpp
@@ -9,6 +9,7 @@
 //-----------------------------------------------------------------------------
 
 #include "Recipe.h"
+#include <stdexcept>
 
 
 //------------------------------------------------------------------------------
@@ -18,8 +19,43 @@ Recipe::Recipe(std::string name) : Command(name)
 
 
 //------------------------------------------------------------------------------
+// try: convert the string from vector[x] to int
+//    if exception -> Error message
+// then convert the int back to strings, to check if it's a float
+//    if yes, then the vector string would be at e.g. 1.2 but the string now 1
+//------------------------------------------------------------------------------
+
 // int Recipe::execute(GameHandler& game, std::vector<std::string>& params)
 int Recipe::execute(std::vector<std::string>& params)
 {
+  if(params.size() == 3)
+  {
+    int lemon, sugar, water;
+    std::string lemon_, sugar_, water_;
+    try
+    {
+      lemon = std::stoi(params[0]);
+      sugar = std::stoi(params[1]);
+      water = std::stoi(params[2]);
+    }
+    catch(std::invalid_argument)
+    {
+      std::cout << "[ERR] Usage: recipe [lemon] [sugar] [water]\n";
+    }
+
+    lemon_ = std::to_string(lemon);
+    sugar_ = std::to_string(sugar);
+    water_ = std::to_string(water);
+
+    if((lemon_ != params[0]) || (sugar_ != params[1]) || (water_ != params[2]))
+    {
+      std::cout << "[ERR] Usage: recipe [lemon] [sugar] [water]\n";
+    }
+
+  }
+  else
+  {
+    std::cout << "[ERR] Usage: recipe [lemon] [sugar] [water]\n";
+  }
   return 0;
 }
