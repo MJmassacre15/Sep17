8c84de18bd7a4bca972f3dcfb582e933ee87f564
Martin Sackl <martin.sackl@student.tugraz.at>
2017-04-25 19:37:20 +0200

    Echo fixed

--- Echo.cpp
+++ Echo.cpp
@@ -21,9 +21,18 @@ Echo::Echo(std::string name) : Command(name)
 // int Echo::execute(GameHandler& game, std::vector<std::string>& params)
 int Echo::execute(std::vector<std::string>& params)
 {
-  for(int i = 0; i < params.size(); i++)
+  int j = params.size();
+  for(int i = 0; i < j; i++)
   {
-    std::cout << params[i] << std::endl;
+    if(i == 0)
+    {
+      std::cout << params[i];
+    }
+    else
+    {
+      std::cout << " " << params[i];
+    }
   }
+  std::cout << "\n";
   return 0;
 }
