c54854b07d3d41d61ab9c5a7b1089ab30cac4829
Paul J. Diwiak <diwiak@student.tugraz.at>
2017-05-03 17:51:03 +0200

    GameData

--- GameData.cpp
+++ GameData.cpp
@@ -4,6 +4,59 @@
 // Authors: Alexander Brunner, Paul J. Diwiak, Martin Sackl
 // Tutor: Roman Walch
 // Group: 15626
-// Created: 02.05.2017
-// Last change: 02.05.2017
+// Created: 03.05.2017
+// Last change: 03.05.2017
 //-----------------------------------------------------------------------------
+#include "GameData.h"
+#include <iostream>
+GameData::GameData(unsigned int lemon_price, unsigned int sugar_price,
+  unsigned int cash, unsigned int lemons, unsigned int sugar) :
+  lemon_price_(lemon_price),
+  sugar_price_(sugar_price),
+  cash_(cash),
+  lemons_(lemons),
+  sugar_(sugar)
+{}
+//setter
+void GameData::setLemonPrice(unsigned int lemon_price)
+{
+  lemon_price_ = lemon_price;
+}
+
+void GameData::setSugarPrice(unsigned int sugar_price)
+{
+  sugar_price_ = sugar_price;
+}
+void GameData::setCash(unsigned int cash) {
+  cash_ = cash;
+}
+void GameData::setLemons(unsigned int lemons)
+{
+  lemons_ = lemons;
+}
+void GameData::setSugar(unsigned int sugar)
+{
+  sugar_ = sugar;
+}
+
+//getter
+unsigned int GameData::getLemonPrice()
+{
+  return lemon_price_;
+}
+unsigned int GameData::getSugarPrice()
+{
+  return sugar_price_;
+}
+unsigned int GameData::getCash()
+{
+  return cash_;
+}
+unsigned int GameData::getLemons()
+{
+  return lemons_;
+}
+unsigned int GameData::getSugar()
+{
+  return sugar_;
+}
--- GameData.h
+++ GameData.h
@@ -1,9 +1,41 @@
+#ifndef GameData_h
+#define GameData_h
+#include <iostream>
 //------------------------------------------------------------------------------
 // Filename: GameData.h
 // Description: header-file for storing the data of the game
 // Authors: Alexander Brunner, Paul J. Diwiak, Martin Sackl
 // Tutor: Roman Walch
 // Group: 15626
-// Created: 02.05.2017
-// Last change: 02.05.2017
+// Created: 03.05.2017
+// Last change: 03.05.2017
 //-----------------------------------------------------------------------------
+class GameData
+{
+  private:
+    unsigned int lemon_price_;
+    unsigned int sugar_price_;
+    unsigned int cash_;
+    unsigned int lemons_;  //number of lemons in the storage
+    unsigned int sugar_;   //amount of sugar in the storage
+
+  public:
+    //constructor
+    GameData(unsigned int lemon_price, unsigned int sugar_price,
+      unsigned int cash, unsigned int lemons, unsigned int sugar);
+
+    //setter
+    void setLemonPrice(unsigned int lemon_price);
+    void setSugarPrice(unsigned int sugar_price);
+    void setCash(unsigned int cash);
+    void setLemons(unsigned int lemons);
+    void setSugar(unsigned int sugar);
+
+    //getter
+    unsigned int getLemonPrice();
+    unsigned int getSugarPrice();
+    unsigned int getCash();
+    unsigned int getLemons();
+    unsigned int getSugar();
+};
+#endif
