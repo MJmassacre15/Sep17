5210bf7311e0baa3eb67d9c084bd507451e49313
Martin Sackl <martin.sackl@student.tugraz.at>
2017-05-07 10:18:18 +0200

    defines in two lines yeah

--- View.h
+++ View.h
@@ -15,19 +15,21 @@
 
 #define   EXIT_0    "Going out of business!\n"
 #define   EXIT_1    "[ERR] Out of memory.\n"
-#define   EXIT_2    "[ERR] Wrong usage: ./basic <price_lemonade> <price_lemon> <price_sugar>\n"
+#define   EXIT_2    "[ERR] Wrong usage: ./basic <price_lemonade> "\
+                    "<price_lemon> <price_sugar>\n"
 
-#define   ERROR_0    "[ERR] Usage: quit\n"
-#define   ERROR_1    "[ERR] Usage: play\n"
-#define   ERROR_2    "[ERR] Usage: recipe [lemon] [sugar] [water]\n"
-#define   ERROR_3    "[ERR] The sum of parts must be 100.\n"
-#define   ERROR_4    "[ERR] Usage: setweather [cover] [precipitation] [temperature] [wind]\n"
-#define   ERROR_5    "[ERR] Wrong parameter.\n"
-#define   ERROR_6    "[ERR] Unknown command.\n"
-#define   ERROR_7    "[ERR] Usage: balance\n"
-#define   ERROR_8    "[ERR] Usage: quote\n"
+#define   ERROR_0   "[ERR] Usage: quit\n"
+#define   ERROR_1   "[ERR] Usage: play\n"
+#define   ERROR_2   "[ERR] Usage: recipe [lemon] [sugar] [water]\n"
+#define   ERROR_3   "[ERR] The sum of parts must be 100.\n"
+#define   ERROR_4   "[ERR] Usage: setweather [cover] [precipitation] "\
+                    "[temperature] [wind]\n"
+#define   ERROR_5   "[ERR] Wrong parameter.\n"
+#define   ERROR_6   "[ERR] Unknown command.\n"
+#define   ERROR_7   "[ERR] Usage: balance\n"
+#define   ERROR_8   "[ERR] Usage: quote\n"
 
-#define   WARN_0     "[WARN] Not enough money. I buy what I can.\n"
+#define   WARN_0    "[WARN] Not enough money. I buy what I can.\n"
 
 
 class View
